f927a3903ac8454396f9ab86d63e5f045d618eb2
jinjoolee1 <jinjoolee126@gmail.com>
2023-04-13 20:17:45 -0500

    profile page updates

--- about.js
+++ about.js
@@ -4,10 +4,15 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	document.querySelector("form").addEventListener("submit");
+	const showAlert = () => {
+	//console.log('form submit');
+	button.addEventListener('click', () => {
+		alert('Success! You have submitted your response');
+		submit = confirm("Success! You have submitted your response!");
+	  });
+	}
 }
-
 
 let form = document.querySelector('#contact');
 
