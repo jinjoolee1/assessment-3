a70a7f26dfe4a4eb3a69b6977f9e81b4a09eb6e6
jinjoolee1 <jinjoolee126@gmail.com>
2023-04-16 15:21:32 -0500

    profile.js updates

---
+++ profile.js
@@ -0,0 +1,15 @@
+document.getElementById("color").addEventListener("click", function () {
+    alert("Purple is my favorite color");
+});
+
+// eventBtn.addEventListener('click', (evt) => {
+//   console.log(evt);
+// });
+document.getElementById("place").addEventListener("click", function () {
+    alert("My own bed!");
+});
+
+document.getElementById("ritual").addEventListener("click", function () {
+    alert("A long, hot shower!");
+});
+
